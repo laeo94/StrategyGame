e5e54e8291ab9a333c58005332b705ebd71ec077
laeo94 <43847578+laeo94@users.noreply.github.com>
2019-02-06 17:39:52 +0100

    Delete ChatEvents.java

--- TP PCR/src/ChatEvents.java
+++
@@ -1,9 +0,0 @@
-
-public interface ChatEvents {
-	public void userListChanged();
-	public void chatMessageSent(String from, String message);
-	public void privateChatMessageSent(String from, String to, String message);
-	public void shutdownRequested();
-	public void roomListChanged();
-
-}
